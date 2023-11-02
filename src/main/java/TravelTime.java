61e2c1f11d7503bcd9adee1016e8d1a9d71dfc0d
Olga Surnina <veehja88@gmail.com>
2023-11-02 15:30:09 +0300

    Initial commit

---
+++ src/main/java/TravelTime.java
@@ -0,0 +1,21 @@
+public class TravelTime {
+
+
+    public static void main(String[] args) {
+
+        final short lengthPath = 54;
+        System.out.println("Расстояние от дома до работы " + lengthPath + " км");
+
+        double averageSpeedTaxi = 32.2;
+        System.out.println("Средняя скорость такси без пробок " + averageSpeedTaxi + " км/ч");
+
+        double travelTime = lengthPath / averageSpeedTaxi;
+        System.out.println("Время в пути без пробок " + travelTime + " ч.");
+
+        double indexRetard = 1.7;
+        System.out.println("Коэффицент замедления транспорта вечером" + indexRetard);
+
+        double travelTimePileup = travelTime * indexRetard;
+        System.out.println("Время в пути с пробками " + travelTimePileup + " ч.");
+    }
+}
